289c74fefa40c4125fe75b6bddc0aa9dbed81828
German Gabriel <elmaildegerman@gmail.com>
2025-05-03 19:49:28 -0400

    v1 luis fer version page

--- src/pages/CopyTrade/LuisFernandoV1.tsx
+++ src/pages/CopyTrade/LuisFernandoV1.tsx
@@ -286,7 +286,7 @@ const LuisFernandoV1Profile = () => {
 							<iframe
 								width="100%"
 								height="100%"
-								src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+								src="https://www.youtube.com/embed/9RSEBKCk3tI?autoplay=1"
 								title="YouTube video player"
 								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
